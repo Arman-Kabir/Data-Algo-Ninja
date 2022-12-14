5f25986edd55ca3abec8725466f62f59288ef48f
Arman-Kabir <armankabir8@gmail.com>
2022-12-14 14:21:26 +0600

    sumUpTo done

--- Intro/2.1-intro.js
+++ Intro/2.1-intro.js
@@ -1,1 +1,22 @@
-// good 
+//  Data is Everywhere
+// Data Structures:  Best way to structure/organize data so that we can easily access,manipulate the data(add,update,delete)
+// Algorithm: Steps to solve  a specific problem
+
+// Sum up to a specific number
+
+function sumUpTo(num) {
+    let sum = 0;
+    for (let i = 0; i < num; i++) {
+        console.log(sum,i);
+        sum += i;
+        console.log(sum);
+    }
+    return sum;
+}
+
+const total = sumUpTo(10);
+console.log(total);
+
+
+
+
