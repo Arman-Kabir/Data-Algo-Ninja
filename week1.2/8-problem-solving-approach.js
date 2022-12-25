dda6b5c456b2cfac2d06768929201e487f3ba55c
Arman-Kabir <armankabir8@gmail.com>
2022-12-25 13:46:39 +0600

    problem solving approach done

---
+++ week1.2/8-problem-solving-approach.js
@@ -0,0 +1,31 @@
+
+
+
+// PROBLEM SOLVING APPROACH
+/** ***** 1.understanding the problem
+ * restate the problem in own words
+ * what are the inputs and outputs
+ * is there enough information to solve the problem
+ * thinking of labelling data(don't use random label like i,j,k,a,b)
+ * checking the example with invalid, empty input
+ * 
+ * *****2. Not down the details like input,output,outcome
+ * 
+ * *****3. Start with naive/brute force approach. First thing that comes into mind. It shows that u'r able to think well and 
+ * critically (you don't need to write this code,just speak about it)
+ *  
+ * 
+ * *****4. Tell them why this approach is not the best
+ * 
+ * *****5. Walk through your approach, comment things and see where you may be able to break things
+ * 
+ * *****6. Before you start coding, walk through your code and write down the steps you are going to follow
+ * 
+ * *****7. breakdown the steps and comment is the ultimate key, rundown of thinking process,steps.
+ *  Breaking down the code will give you the extra advantage
+ *  
+ * 
+ * *****8. start coding. (simplify as necessary)
+ */
+
+
