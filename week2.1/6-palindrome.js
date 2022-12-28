7e3cfcf16487dbc042092ce8ecdc3d78d8be14d6
Arman-Kabir <armankabir8@gmail.com>
2022-12-28 15:25:37 +0600

    palindrome done

--- week2.1/6-palindrome.js
+++ week2.1/6-palindrome.js
@@ -1,1 +1,24 @@
-// 
+// Check provided string is palindrome.
+// if palindrome return true otherwise false
+
+// non-alphanumeric character should be ignored
+
+// palindrome("race car") should return true
+// palindrome("not a palindrome") should return false
+
+function pallindrome(str) {
+    // [0-9A-Za-z] - these are alphanumeric character. --- [\w].
+    //  All except these are non-alphanumeric. --- [\W]... ---> []-> set
+
+    const re = /[\W_]/g
+
+    const normalizeStr = str.toLowerCase().replace(re,'');
+    const reverseStr = normalizeStr.split('').reverse().join('');
+
+    return normalizeStr === reverseStr;
+
+    console.log(normalizeStr,reverseStr);
+}
+
+console.log(pallindrome("race _car"));
+console.log(pallindrome("not a palindrome"));
