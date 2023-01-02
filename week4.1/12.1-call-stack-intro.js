439e5876fbec912078363445bf559230f114a01b
Arman-Kabir <armankabir8@gmail.com>
2023-01-02 14:08:55 +0600

    call stack done

---
+++ week4.1/12.1-call-stack-intro.js
@@ -0,0 +1,21 @@
+// Recursion - Works as integral parts of other algorithms.
+// Funntion which calls itself
+
+// Call Stack -- Our function runs or executes in call stack step by step
+// everything in js runs in the context of a function
+
+function one() {
+    return 1
+}
+
+function two() {
+    return 2
+}
+
+function three() {
+    return 3
+}
+
+console.log(one())
+console.log(two())
+console.log(three())
