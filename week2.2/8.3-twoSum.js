e42da3864bb91b675c5ba4806eb28087d60d4f37
Arman-Kabir <armankabir8@gmail.com>
2023-01-04 15:27:08 +0600

    two sum tashmap done

---
+++ week2.2/8.3-twoSum.js
@@ -0,0 +1,33 @@
+// Two Sum 
+
+// time complexity -- O(n^2)
+// space complexity -- O()
+// function twoSum(nums, target) {
+//     for (let i = 0; i < nums.length-1; i++) {
+//         for (let j = i + 1; j < nums.length; j++) {
+//             if (nums[i] + nums[j] === target) {
+//                 return [i, j];
+//             }
+//         }
+//     }
+// }
+// console.log(twoSum([3, 2, 4,3,8], 11)); 
+
+
+function twoSum(nums, target) {
+    let hashTable = {}
+    for (let i = 0; i < nums.length; i++) {
+        const want = target - nums[i];
+        if (want in hashTable) {
+            return [hashTable[want],i]
+        }
+        hashTable[nums[i]] = i;
+    }
+}
+console.log(twoSum([3, 2, 4], 6));
+
+
+
+
+
+
