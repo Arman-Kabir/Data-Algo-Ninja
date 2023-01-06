0bf7422c2a6b8a2e20a79422a3c5ae96cdeae69c
Arman-Kabir <armankabir8@gmail.com>
2023-01-06 14:30:54 +0600

    firstUniqueChar done

---
+++ week2.2/8.4-subArraySum.js
@@ -0,0 +1,13 @@
+//  subArraySum lc- 560
+
+function subArraySum(nums, k) {
+    const hashMap = {
+        0: 1
+    }
+    for(let i = 0;i<nums.length;i++){
+        
+    }
+}
+
+console.log(subArraySum([1, 1, 1], 2));
+console.log(subArraySum([1, 2, 3], 3));
