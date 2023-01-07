a4147cba0693b05517319a359ac3c2f5b18bf215
Arman-Kabir <armankabir8@gmail.com>
2023-01-07 11:28:55 +0600

    sumZero done

---
+++ week3.1/9.1.js
@@ -0,0 +1,25 @@
+// Nested loops make O(n^2). To make it efficient O(n) -- we use hashTable.
+
+// sumZero
+
+function sumZero(arr) {
+    let left = 0;
+    let right = arr.length - 1;
+    console.log(left,right);
+    while (left < right) {
+        let sum = arr[left] + arr[right];
+        if (sum === 0) {
+            return [arr[left], arr[right]];
+        }
+        if (sum > 0) {
+            right--
+        }
+        if (sum < 0) {
+            left++
+        }
+    }
+    return undefined;
+}
+
+console.log(sumZero([-3, -2, -1, 0, 1, 2, 3]));
+                //
